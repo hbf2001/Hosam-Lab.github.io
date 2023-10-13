d097a558f51587e3563b3c02ce0b8f0bd7f0a06a
hbf2001 <145095548+hbf2001@users.noreply.github.com>
2023-10-13 21:22:11 +0400

    Added JS File

---
+++ week3/gallery.js
@@ -0,0 +1,32 @@
+let currentSlide = document.getElementsByClassName("numberText").textContent;
+let slidesDiv = document.querySelectorAll(".mySlides");
+let images = slidesDiv.getElementsByName("img");
+
+const nextButton = document.querySelectorAll(".next");
+const prevButton = document.querySelectorAll(".prev")
+
+nextButton.addEventListener("click", function() {
+
+    if(currentSlide === 8)
+        nextButton.disabled = true;
+    else
+        nextButton.disabled = false;
+
+
+    //Display mySlides that has number text that respective number
+    //increment current slide
+    images.style.display = "block";
+    currentSlide++;
+
+});
+
+prevButton.addEventListener("click", function(){
+
+    if(currentSlide === 1)
+        prevButton.disabled = true;
+    else
+        prevButton.diabled = false;
+});
+
+
+
