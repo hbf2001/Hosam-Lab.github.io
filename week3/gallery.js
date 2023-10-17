70e4f3739ba37d414183d898675e6e920371b333
hbf2001 <145095548+hbf2001@users.noreply.github.com>
2023-10-17 21:45:14 +0400

    added comments

--- week3/gallery.js
+++ week3/gallery.js
@@ -1,3 +1,4 @@
+//created an array that holds the images for the slideshow
 let images = [];
 images[0] = "images/imageSlider1.jpeg";
 images[1] = "images/imageSlider2.jpeg";
@@ -8,7 +9,7 @@ images[5] = "images/imageSlider6.jpeg";
 images[6] = "images/imageSlider7.jpeg";
 images[7] = "images/imageSlider8.avif";
 
-let i = 0;//var to start image display.
+let i = 0;//variable to start image display.
 const time = 3000;//time set to 3000 ms or 3 s.
 
 //Function for slide show.
@@ -16,14 +17,14 @@ function imageSlider() {
     document.slide.src = images[i];
 
     if(i < images.length - 1 ) 
-        i++;
+        i++;//increments i to preform the slideshow
 
-    else 
+    else //resets to 0 after the last image display
         i = 0;
 
     setTimeout("imageSlider()", time);
 }
-
+//runs the function when page loads as requested in the lab sheet.
 window.onload = imageSlider;
 
 
